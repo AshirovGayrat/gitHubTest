c476a59c04b034c32935f6fe779a7225f883b580
zozo9979 <azizkabirov10@gmail.com>
2025-01-21 16:51:29 +0500

    no no no

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,6 +12,8 @@ public class Main {
 
         System.out.println(" Man yana yozdim ");
 
+        System.out.println("Aziz push qldi");
+
 
     }
 }
---
+++ src/main/java/org/example/nonono.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class nonono {
+    public static void main(String[] args) {
+        System.out.println("Aziz push qldi");
+    }
+}
